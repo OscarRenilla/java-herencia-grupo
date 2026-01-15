20abcc9ae8fae8a6df805db09e34af388f914f35
Óscar Reni <oscar06renilla@gmail.com>
2026-01-15 09:55:35 +0000

    Actualización de clases Desarrollador y Manager

--- Manager.java
+++ Manager.java
@@ -1,8 +1,9 @@
 public class Manager extends Empleado {
     protected int personasACargo; 
 
-    public Manager(int personasACargo){
-        super(personasACargo);
+    public Manager(String dni, String nombre, int edad, double salarioBase, int antiguedadAnios, int personasACargo){
+        super(dni, nombre, edad, salarioBase, antiguedadAnios);
+        this.personasACargo = personasACargo;
     }
 
     public int getPersonasACargo(){
@@ -13,7 +14,11 @@ public class Manager extends Empleado {
         this.personasACargo = personasACargo; 
     }
 
-    public double calcularSalario(){
-        return calcularSalario;
+    @Override 
+    public double calcularSalario() {
+        double plusResponsabilidad = personasACargo * 100;
+        double plusAntiguedad = antiguedadAnios * 75;
+        return salarioBase + plusResponsabilidad + plusAntiguedad;      
     }
+
 }
